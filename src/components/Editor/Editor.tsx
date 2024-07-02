7ba59b18804c9d41c50d590e51d2361a5ad5f2cc
Hixo <hixohixo32@gmail.com>
2024-07-02 13:52:55 +0200

    refactor: add useEditor hook

--- src/components/Editor/Editor.tsx
+++ src/components/Editor/Editor.tsx
@@ -7,68 +7,21 @@ import { BlockNoteView } from "@blocknote/mantine";
 import { useCreateBlockNote } from "@blocknote/react";
 import { editNote } from "@/actions/notes";
 import { useDebouncedCallback } from 'use-debounce';
+import { useEditor } from "@/hooks/useEditor";
 
 type EditorProps = {
-  noteId: string;
-  initialContent?: string | null;
-};
-
-type Block = {
-  id: string;
-  type: string;
-  props: Record<string, boolean | number | string>;
-  content: InlineContent[] | TableContent<any> | undefined;
-  children: Block[];
-};
-
-type Link = {
-  type: "link";
-  content: StyledText[];
-  href: string;
-};
-
-type StyledText = {
-  type: "text";
-  text: string;
-  styles: Styles<any>;
-};
-
-type InlineContent = Link | StyledText;
-
-function isBlock(obj: any): obj is Block {
-  return (
-    typeof obj === "object" &&
-    obj !== null &&
-    typeof obj.id === "string" &&
-    typeof obj.type === "string" &&
-    typeof obj.props === "object" &&
-    Array.isArray(obj.children)
-  );
-}
-
-function isArrayOfBlocks(arr: any): arr is Block[] {
-  return Array.isArray(arr) && arr.every(isBlock) && arr.length >= 1;
-}
+    noteId: string;
+    initialContent?: string | null;
+  };
 
 export function Editor({ noteId, initialContent }: EditorProps) {
-  const parsedContent =
-    initialContent && isArrayOfBlocks(JSON.parse(initialContent))
-      ? JSON.parse(initialContent)
-      : null;
-
-  const editor = useCreateBlockNote({
-    initialContent: parsedContent,
-  });
-
-  const debounced = useDebouncedCallback(async () => {
-    const currentBlocks = editor.document;
-    await editNote(noteId, JSON.stringify(currentBlocks));
-  }, 300);
+  
+  const { debounced, editor } = useEditor(noteId, initialContent ?? null);
 
   return (
     <BlockNoteView
       onChange={() => debounced()}
-      className="w-full min-h-screen py-12 px-4 z-0"
+      className="w-full py-12 px-4 z-0 overflow-y-scroll"
       editor={editor}
     ></BlockNoteView>
   );
---
+++ src/hooks/useEditor.tsx
@@ -0,0 +1,61 @@
+import { editNote } from "@/actions/notes";
+import { Styles, TableContent } from "@blocknote/core";
+import { useCreateBlockNote } from "@blocknote/react";
+import { useDebouncedCallback } from "use-debounce";
+
+
+  
+  type Block = {
+    id: string;
+    type: string;
+    props: Record<string, boolean | number | string>;
+    content: InlineContent[] | TableContent<any> | undefined;
+    children: Block[];
+  };
+  
+  type Link = {
+    type: "link";
+    content: StyledText[];
+    href: string;
+  };
+  
+  type StyledText = {
+    type: "text";
+    text: string;
+    styles: Styles<any>;
+  };
+  
+  type InlineContent = Link | StyledText;
+  
+  function isBlock(obj: any): obj is Block {
+    return (
+      typeof obj === "object" &&
+      obj !== null &&
+      typeof obj.id === "string" &&
+      typeof obj.type === "string" &&
+      typeof obj.props === "object" &&
+      Array.isArray(obj.children)
+    );
+  }
+  
+  function isArrayOfBlocks(arr: any): arr is Block[] {
+    return Array.isArray(arr) && arr.every(isBlock) && arr.length >= 1;
+  }
+
+export function useEditor(noteId: string, initialContent?: string | null) {
+    const parsedContent =
+    initialContent && isArrayOfBlocks(JSON.parse(initialContent))
+      ? JSON.parse(initialContent)
+      : null;
+
+    const editor = useCreateBlockNote({
+        initialContent: parsedContent,
+      });
+    
+      const debounced = useDebouncedCallback(async () => {
+        const currentBlocks = editor.document;
+        await editNote(noteId, JSON.stringify(currentBlocks));
+      }, 300);
+
+      return {editor, debounced}
+}
