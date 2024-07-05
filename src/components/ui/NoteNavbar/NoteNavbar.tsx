d3a29c60d62322ce1a19c5a46e595719926137ff
Hixo <hixohixo32@gmail.com>
2024-07-05 10:34:01 +0200

    feat: add saving with enter

--- src/components/ui/NoteNavbar/NoteNavbar.tsx
+++ src/components/ui/NoteNavbar/NoteNavbar.tsx
@@ -12,15 +12,19 @@ type NoteNavbarProps = {
 export function NoteNavbar({ title, noteId }: NoteNavbarProps) {
     const [noteTitle, setNoteTitle] = useState(title);
     const [editing, setEditing] = useState(false);
-  return (
+
+    const handleEdit = async() => {
+      if(!noteTitle) return;
+      editNoteTitle(noteId, noteTitle);
+      setEditing(false)
+    }
+   return (
     <div className="w-full h-12 flex items-center px-4 ml-60 z-10 fixed top-0 border-b bg-[#161616] border-b-[#303030]">
      {editing ? <div className="flex gap-2 items-center">
-        <Input value={noteTitle} onChange={(evt) => setNoteTitle(evt.target.value)} placeholder="Note title" aria-label="Note title"  />
-        <Button onClick={async() => {
-            if(!noteTitle) return;
-            editNoteTitle(noteId, noteTitle);
-            setEditing(false)
-        }} color="primary">Save title</Button>
+        <Input onKeyUp={async(evt) => {
+          if(evt.code === "Enter") await handleEdit();
+        }} value={noteTitle} onChange={(evt) => setNoteTitle(evt.target.value)} placeholder="Note title" aria-label="Note title"  />
+        <Button onClick={async() => await handleEdit()} color="primary">Save title</Button>
      </div> :  <p onClick={() => setEditing(true)} className="text-white text-lg font-semibold">{noteTitle}</p>}
     </div>
   );
