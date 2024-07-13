29ae7b99da7e5e268b423fd9d9340da7674610af
Hixo <hixohixo32@gmail.com>
2024-07-13 17:53:06 +0200

    refactor: remove overflow scroll

--- src/components/NotesList/NotesList.tsx
+++ src/components/NotesList/NotesList.tsx
@@ -8,7 +8,7 @@ export async function NotesList() {
   const userNotes = await getUserNotes(session!.user!.email!);
 
   return (
-    <nav className="flex flex-col gap-4 overflow-y-scroll">
+    <nav className="flex flex-col gap-4">
       {userNotes.map((note) => (
         <SingleNote {...note} key={note.id} />
       ))}
