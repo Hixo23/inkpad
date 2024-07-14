e4a303b7cf992452f4825e950c2ea11baaf09a28
Hixo <hixohixo32@gmail.com>
2024-07-14 11:04:18 +0200

    refactor: remove unused file extensions

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,9 +3,8 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{ts,tsx}",
+    "./src/app/**/*.{ts,tsx}",
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
