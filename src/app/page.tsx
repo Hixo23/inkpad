b686f51ee0cf8563ae69e07ec6a916c4b8b3df3f
Hixo <hixohixo32@gmail.com>
2024-07-17 22:53:41 +0200

    feat: add bg pattern

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Features } from '@/components/Sections/Features'
 import { Hero } from '@/components/Sections/Hero'
 import { Navbar } from '@/components/ui/Navbar/Navbar'
 
@@ -6,6 +7,7 @@ export default function Home() {
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
             <Navbar />
             <Hero />
+            <Features />
         </main>
     )
 }
