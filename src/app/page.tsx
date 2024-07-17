cec48a01021296210d0dd9fc434e1157de5c9fb5
Hixo <hixohixo32@gmail.com>
2024-07-17 23:02:55 +0200

    refactor: remove padding from page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { Navbar } from '@/components/ui/Navbar/Navbar'
 
 export default function Home() {
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <main className="flex min-h-screen flex-col items-center justify-between">
             <Navbar />
             <Hero />
             <Features />
