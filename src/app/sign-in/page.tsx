b84a45b6c96b6bed59a21c8e4ef7c454ccc29a85
hixo <hixohixo32@gmail.com>
2024-08-16 11:04:16 +0200

    fix: remove static height from sign in form

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -2,8 +2,8 @@ import { CredentialsButtons } from '@/components/CredentialsButtons/CredentialsB
 
 export default function signInPage() {
     return (
-        <main className="w-screen min-h-screen flex items-center justify-center">
-            <div className="flex flex-col items-center   bg-[#303030] h-96 lg:w-1/2 w-2/3 gap-6 rounded-xl p-6">
+        <main className=" min-h-screen flex items-center justify-center">
+            <div className="flex flex-col items-center bg-[#303030] lg:w-1/2 w-2/3 gap-6 rounded-xl p-4">
                 <div className="flex flex-col items-center py-4 text-white text-center gap-2">
                     <h1 className="text-4xl font-bold">Hello!</h1>
                     <p className="text-gray-400 text-sm">
