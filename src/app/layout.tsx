73ce6e752d5aada85c427c70597c198d4e0ca3db
hixo <hixohixo32@gmail.com>
2024-08-16 20:08:31 +0200

    fix: add dark class to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-            <body className={poppins.variable}>
+            <body className={`${poppins.variable} dark`}>
                 <Providers>{children}</Providers>
             </body>
         </html>
