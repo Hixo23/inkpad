e5a48a96ac28b64b23471b9a6f6bdcf40e28f618
Hixo <hixohixo32@gmail.com>
2024-09-24 13:00:00 +0200

    refactor: add redirect to last note from /notes

--- src/app/notes/page.tsx
+++ src/app/notes/page.tsx
@@ -1,7 +1,12 @@
+import { redirect } from 'next/navigation'
+import { getUserNotes } from '@/actions/notes'
+import { auth } from '@/lib/auth'
+import { Loading } from '@/components/ui/Loading/Loading'
+
 export default async function NotesPage() {
-    return (
-        <div className="w-screen min-h-screen text-white bg-[#161616] flex justify-center items-center py-4">
-            <p>Create new Note on sidebar!</p>
-        </div>
-    )
+    const session = await auth()
+    if(!session?.user?.email) return null;
+    const notes = await getUserNotes(session.user.email);
+    redirect(`/notes/${notes[0].id}`)
+    return <Loading />
 }
